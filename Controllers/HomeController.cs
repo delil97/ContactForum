cc37262d3fbe40814179efe14b2f6b239335a197
delil97 <58556097+delil97@users.noreply.github.com>
2020-11-02 10:39:48 +0100

    Completed

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ContactForSpacess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactForSpacess.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var mail = new MailMessage();
+                mail.To.Add(new MailAddress(model.SenderEmail));
+                mail.Subject = "Your Email Subject";
+                mail.Body = string.Format("<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>", model.SenderName, mail.Sender, model.Message);
+                mail.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(mail);
+                    return RedirectToAction("SuccessMessage");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult SuccessMessage()
+        {
+            return View();
+        }
+    }
+}
